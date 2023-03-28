1e720a9b48e9aedaafd4c12ca4c99ee38aa929f9
jauxan <jauxan123@gmail.com>
2023-03-28 20:42:18 +0200

    v1

--- app/identidock.py
+++ app/identidock.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'Hello World from Ciberseguridad!\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
